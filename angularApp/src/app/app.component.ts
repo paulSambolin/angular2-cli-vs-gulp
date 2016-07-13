4197f5a228c19837e802e7bce8899f89c9ce83d1
Paul Sambolin <paulsambolin@PSAMBOLIN-MBP-NY.local>
2016-07-13 09:21:39 -0400

    initial commit of testGulp branch

--- angularApp/src/app/app.component.ts
+++ angularApp/src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+
+import {Component, OnInit} from "@angular/core";
+/*
+import {RouteConfig, RouterLink, ROUTER_DIRECTIVES} from "@angular/router-deprecated";
+
+import {TaskListComponent} from "./todo/components/task-list.component";
+import {AboutComponent} from "./about/components/about.components";
+*/
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+    selector: "app",
+    templateUrl: "./app/app.html",
+    styleUrls: ['./app/app.component.css']
+    /*
+    directives: [TaskListComponent, AboutComponent, RouterLink, ROUTER_DIRECTIVES]*/
 })
-export class AppComponent {
-
+/*
+@RouteConfig([
+    {path: '/tasks', component: TaskListComponent, as: 'TaskList'},
+    {path: '/about', component: AboutComponent, as: 'About'}
+])
+*/
+export class AppComponent implements OnInit {
+    ngOnInit() {
+        console.log("Application component initialized ...");
+    }
 }
